acb034757ec92f7d4f08897251f9b32037088fa4
anilkumar2121 <anilcruel2121@gmail.com>
2024-03-03 19:40:37 +0530

    sixth

--- src/components/treeview/TreeView.jsx
+++ src/components/treeview/TreeView.jsx
@@ -5,8 +5,13 @@ import "./Styles.css";
 
 const TreeView = ({ menus = [] }) => {
   return (
-    <div className="treeViewContainer">
-      <ViewList list={menus} />
+    <div>
+      <h1 style={{ color: "red" }}>
+        <center>Sixth Project About Recurrsion of Data</center>
+      </h1>
+      <div className="treeViewContainer">
+        <ViewList list={menus} />
+      </div>
     </div>
   );
 };
